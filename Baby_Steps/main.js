14c36a461918283699107e08e6c2699b6a367d82
git-sahil36 <sahilpriyadarshivpn@gmail.com>
2026-03-06 19:48:51 +0530

    Linux Install

---
+++ Baby_Steps/main.js
@@ -0,0 +1,62 @@
+function greet() {
+    console.log("Hello World");
+}
+
+setTimeout(greet, 1*1000)
+
+function setTimeout
+
+
+function setTimeout(arithmeticFinalFunction, duration){
+
+    sleep duration(20)  
+}
+
+function sub() {
+    a-b;
+}
+
+function sum() {
+    a+b;
+}
+
+function mul(a,b,c) {
+    a * b * c;
+}
+
+function calculateSum(a,b) {
+    a+b;   
+}
+
+console.log(sum(1,5,displayResult))
+
+function displayResultPassive(data) {
+    console.log("Sum's result is : " + data);
+}
+console.log(sum(1,5,displayResult))
+
+function doSomethiing(a,b,fn) {
+    if(type == "sum") {
+        return a + b;
+    }    
+}
+
+function hello(parameter_1, parameter_2) {
+    console.log(parameter_1, parameter_2)    
+}
+setTimeout(hello, 2000, parameter_1 , parameter_2)
+
+for(let i = 0; i<=1000; i = i+1){
+    answer = answer + i;
+}
+
+console.log(answer);
+
+const person_1 = "Sahil";
+const gender_1 = "Male";
+
+const person_2 = "Harkirath";
+const gender_2 = "Male";
+
+const person_Array = ["Sahil","Harkirath"];
+const gender_Array = ["Male","Male"];
